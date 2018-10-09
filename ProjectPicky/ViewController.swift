4384bc6ee7960b602046198a58524374f08fef8f
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-10-09 13:03:19 -0400

    initial commit

---
+++ ProjectPicky/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectPicky
+//
+//  Created by Mitchell Socia on 10/9/18.
+//  Copyright © 2018 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
