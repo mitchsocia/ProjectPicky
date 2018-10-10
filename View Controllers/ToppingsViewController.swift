5332364e8a50715e0a6c8c12fe97e8f9c545c468
Mitchell Socia <mitch.socia@detroitlabs.com>
2018-10-10 15:04:42 -0400

    Started setting up table view, changed file names

---
+++ View Controllers/ToppingsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ToppingsViewController.swift
+//  ProjectPicky
+//
+//  Created by Mitchell Socia on 10/10/18.
+//  Copyright © 2018 Mitchell Socia. All rights reserved.
+//
+
+import UIKit
+
+class ToppingsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+
+}
